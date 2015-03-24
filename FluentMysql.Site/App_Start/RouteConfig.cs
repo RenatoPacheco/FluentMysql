2e6014ed317a217ff23e11d5c335221c590de370
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2015-03-24 19:11:38 -0300

    Organizando o layout base e adicionando novas fontes.

--- FluentMysql.Site/App_Start/RouteConfig.cs
+++ FluentMysql.Site/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace FluentMysql.Site
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "FluentMysql.Site.Controllers" }
             );
         }
     }
