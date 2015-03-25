75808a440817660ad84e2c7b6e9e54f19c5dfc7e
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2015-03-25 17:58:15 -0300

    Implementado upload.

--- FluentMysql.Site/Areas/Admin/Controllers/ArtigoController.cs
+++ FluentMysql.Site/Areas/Admin/Controllers/ArtigoController.cs
@@ -8,15 +8,39 @@ using FluentMysql.Site.Helpers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace FluentMysql.Site.Areas.Admin.Controllers
 {
-    [AuthorizeUser(Nivel = new Nivel[] { Nivel.Usuario })]
+    [AuthorizeUser(Nivel = new Nivel[] { Nivel.Operador })]
     public class ArtigoController : Controller
     {
+        public ActionResult Upload(UploadForm dados = null)
+        {
+            if (ModelState.IsValid)
+            {
+                string dir = string.Format("/Images/{0}/{1}/{2}", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+                
+                if (!Directory.Exists(Server.MapPath(string.Format("~{0}", dir))))
+                        Directory.CreateDirectory(Server.MapPath(string.Format("~{0}", dir)));
+                
+                dados.Upload.SaveAs(Server.MapPath(string.Format("~{0}/{1}", dir, dados.Upload.FileName)));
+
+                ViewBag.Url = string.Format("{0}/{1}", dir, dados.Upload.FileName);
+                ViewBag.Mensagem = "Arquivo enviado com sucesso";
+            }
+            else
+            {
+                ViewBag.Url = "";
+                ViewBag.Mensagem = "Ocorreu um erro ao enviar o arquivo";
+            }
+
+            return View(dados);
+        }
+
         public ActionResult Index(FiltroForm filtro = null)
         {
             IList<Artigo> lista = new List<Artigo>();
--- FluentMysql.Site/Scripts/Areas/Admin/artigo.altera.js
+++ FluentMysql.Site/Scripts/Areas/Admin/artigo.altera.js
@@ -2,4 +2,25 @@
     $('#Hashtag').tagsinput({
         confirmKeys: [13, 188, 32]
     });
+
+    $('textarea.basic').each(function (index) {
+    }).ckeditor(function () {
+        // Instance loaded callback.
+    }, {
+        //contentsCss: ['/Content/css/normalize.css', '/Content/css/template-editor.min.css', '/Content/css/template-ckeditor.css'],
+        //on: { change: EventoChange, contentDom: EventoChange, paste: EventoPaste },
+        pasteFromWordPromptCleanup: true,
+        pasteFromWordRemoveFontStyles: true,
+        pasteFromWordNumberedHeadingToList: true,
+        pasteFromWordRemoveStyles: true,
+        ignoreEmptyParagraph: true,
+        removeFormatAttributes: true,
+        toolbar: [
+            ['Source', '-', 'Bold', 'Italic', 'Underline', 'Subscript', 'Superscript', '-', 'RemoveFormat'], ['NumberedList', 'BulletedList', '-', 'Indent', 'Outdent', 'Blockquote', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-'], ['Image']
+        ],
+        filebrowserBrowseUrl: 'browser/browse.php',
+        filebrowserUploadUrl: '/Admin/Artigo/Upload',
+        filebrowserImageWindowWidth: '640',
+        filebrowserImageWindowHeight: '480'
+    });
 });
