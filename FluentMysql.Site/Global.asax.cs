17f7d6327f333a8235db3b121a445de335cfd27b
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2015-04-02 16:03:51 -0300

    Customizado as telas de erro da aplicação.

--- FluentMysql.Site/Global.asax.cs
+++ FluentMysql.Site/Global.asax.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using FluentMysql.Site.Controllers;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -17,6 +20,46 @@ namespace FluentMysql.Site
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AutoMapperConfig.RegisterAutoMappers();
+        }
+
+        protected void Application_Error()
+        {
+            Configuration configuration = WebConfigurationManager.OpenWebConfiguration("~/Web.config");
+            CustomErrorsSection customErrorsSection = (CustomErrorsSection)configuration.GetSection("system.web/customErrors");
+            if (customErrorsSection.Mode == CustomErrorsMode.Off) return;
+
+            var exception = Server.GetLastError();
+            Server.ClearError();
+            var httpException = exception as HttpException;
+
+            //Logging goes here
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error";
+            routeData.Values["action"] = "Index";
+
+            if (httpException != null)
+            {
+                if (httpException.GetHttpCode() == 404)
+                {
+                    routeData.Values["action"] = "NotFound";
+                }
+                Response.StatusCode = httpException.GetHttpCode();
+            }
+            else
+            {
+                Response.StatusCode = 500;
+            }
+
+            // Avoid IIS7 getting involved
+            Response.TrySkipIisCustomErrors = true;
+
+            Response.ContentType = "text/html";
+
+            //Executando o controller Error
+            IController errorsController = new ErrorController();
+            HttpContextWrapper wrapper = new HttpContextWrapper(Context);
+            var rc = new RequestContext(wrapper, routeData);
+            errorsController.Execute(rc);
         }
     }
 }
