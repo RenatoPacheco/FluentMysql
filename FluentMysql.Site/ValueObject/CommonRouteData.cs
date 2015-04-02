e8843291b19a3d89a35cbe7c7e840aaa13358b99
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2015-04-02 19:31:18 -0300

    Corrigindo o nome da  pasta ValueObject

--- FluentMysql.Site/ValueObject/CommonRouteData.cs
+++ FluentMysql.Site/ValueObject/CommonRouteData.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
-namespace FluentMysql.Site.ObjectValue
+namespace FluentMysql.Site.ValueObject
 {
     public class CommonRouteData
     {
