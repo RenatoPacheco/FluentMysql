5e9dfdbcbb6191353721737d473a4aac27913d00
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2015-04-24 15:33:05 -0300

    Adicionado dois factorys para simplificar algumas consultas.

---
+++ FluentMysql.Infrastructure/Factory/FindFactory.cs
@@ -0,0 +1,28 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentMysql.Infrastructure.Factory
+{
+    public static class FindFactory
+    {
+        public static T Find<T>(object key) where T : class,new()
+        {
+            using (Connection connection = new Connection())
+            {
+                using (ISession session = connection.Session)
+                {
+                    return (T)session.Get<T>(key);
+                }
+            }
+        }
+
+        public static T Find<T>(object key, ISession session) where T : class,new()
+        {
+            return (T)session.Get<T>(key);
+        }
+    }
+}
---
+++ FluentMysql.Infrastructure/Factory/QueryFactory.cs
@@ -0,0 +1,45 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentMysql.Infrastructure.Factory
+{
+    public static class QueryFactory
+    {
+        public static IQueryable<T> Query<T>() where T : class,new()
+        {
+            using (Connection connection = new Connection())
+            {
+                using (ISession session = connection.Session)
+                {
+                    return session.CreateCriteria<T>().List<T>().AsQueryable();
+                }
+            }
+        }
+
+        public static IQueryable<T> Query<T>(ISession session) where T : class,new()
+        {
+            return session.CreateCriteria<T>().List<T>().AsQueryable();
+        }
+
+        public static IQueryable<T> Query<T>(ISession session, Expression<Func<T, bool>> where) where T : class,new()
+        {
+            return session.CreateCriteria<T>().List<T>().AsQueryable<T>().Where<T>(where);
+        }
+
+        public static IQueryable<T> Query<T>(Expression<Func<T, bool>> where) where T : class,new()
+        {
+            using (Connection connection = new Connection())
+            {
+                using (ISession session = connection.Session)
+                {
+                    return session.CreateCriteria<T>().List<T>().AsQueryable<T>().Where<T>(where);
+                }
+            }
+        }
+    }
+}
